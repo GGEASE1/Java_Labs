5afeba517c0058022f14e1e4a0dd87e912de8f3d
GGEASE1 <kazantsev.2004.06@gmail.com>
2024-12-15 17:05:41 +0500

    test

--- src/Lab2/Ex21.java
+++ src/Lab2/Ex21.java
@@ -1,6 +1,24 @@
 package Lab2;
 
+import java.util.Scanner;
+
 public class Ex21
 {
-    //Проверка
+
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = in.nextInt();
+        if (number % 3 == 0)
+        {
+            System.out.println("Число делится на 3");
+        }
+        else
+        {
+            System.out.println("Число не делится на 3");
+        }
+
+    }
+
 }
