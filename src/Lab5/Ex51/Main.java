84a566d9273b42c096822c696cf2ecd1d76e47b7
kazan <kazantsev.06.2004@gmail.com>
2025-01-18 23:48:28 +0500

    Complete

--- src/Lab5/Ex51/Main.java
+++ src/Lab5/Ex51/Main.java
@@ -6,9 +6,13 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите символ: ");
+        char symbol = in.next().charAt(0);
+
         Writetest test = new Writetest();
 
-        test.gettext('a');//Тут вводим символ
+        test.gettext(symbol);//Тут вводим символ
 
         System.out.println(test.getcode());
 
