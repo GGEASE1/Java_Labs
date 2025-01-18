a2fe749e459a54bb890a00071ebea2f0b557976f
kazan <kazantsev.06.2004@gmail.com>
2025-01-18 23:48:23 +0500

    Complete

---
+++ src/Lab5/Ex54/Main.java
@@ -0,0 +1,22 @@
+package Lab5.Ex54;
+
+import java.util.Scanner;
+
+public class Main
+{
+        public static void main(String[] args)
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите число: ");
+            int number = in.nextInt();
+            System.out.print("Введите символ: ");
+            char symbol = in.next().charAt(0);
+            System.out.print("Введите число double: ");
+            double number_double = in.nextDouble();
+
+            Test test = new Test();
+
+            test.setFieldAndNumber(symbol, number);
+            test.setFieldAndNumberDouble(number_double);
+        }
+}
