e68884d730a7cef0c6145863b3371ca173ca664b
kazan <kazantsev.06.2004@gmail.com>
2025-01-19 14:54:31 +0500

    Complete

---
+++ src/Lab6/Ex63/Main.java
@@ -0,0 +1,20 @@
+package Lab6.Ex63;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int five = 6;
+        int six = 30;
+        int seven = 62;
+        Test test = new Test();
+        Test.printMaxMinAvg(two);
+        Test.printMaxMinAvg(two, three);
+        Test.printMaxMinAvg(two, three, four);
+        Test.printMaxMinAvg(two, three, four, five);
+        Test.printMaxMinAvg(three, four, five, six);
+        Test.printMaxMinAvg(four, five, six, seven);
+    }
+}
