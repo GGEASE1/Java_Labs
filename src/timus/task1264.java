8aabf490fc820c54c4dd9a8ef80adbe359d30c94
kazan <kazantsev.06.2004@gmail.com>
2025-01-19 15:21:47 +0500

    Complete

---
+++ src/timus/task1264.java
@@ -0,0 +1,17 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1264
+{
+    public static void main(String[] args)
+    {
+            Scanner in = new Scanner(System.in);
+            PrintWriter out = new PrintWriter(System.out);
+            int number1 = in.nextInt();
+            int number2 = in.nextInt();
+            out.print(number1 * (number2+1));
+            out.flush();
+    }
+}
---
+++ src/timus/task1877.java
@@ -0,0 +1,33 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1877
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int padlock_1 = in.nextInt();
+        int padlock_2 = in.nextInt();
+        String result = "no";
+        int first_password_robber = 0;
+        for (;first_password_robber < 9999; first_password_robber++)
+        {
+            if (padlock_1 == first_password_robber)
+            {
+                result = "yes";
+                break;
+            }
+            else
+            {
+                int temp = padlock_1;
+                padlock_1 = padlock_2;
+                padlock_2 = temp;
+            }
+        }
+        out.print(result);
+        out.flush();
+    }
+}
