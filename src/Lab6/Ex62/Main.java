ce6d529538c365d41486912fe477514f23742d69
kazan <kazantsev.06.2004@gmail.com>
2025-01-19 14:53:09 +0500

    Complete

---
+++ src/Lab6/Ex62/Main.java
@@ -0,0 +1,14 @@
+package Lab6.Ex62;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int count = 6;
+        Test test = new Test();
+        for (int i=0; i < count; i++)
+        {
+            Test.printNumberFieldAndIncrement();
+        }
+    }
+}
---
+++ src/Lab6/Ex62/Test.java
@@ -0,0 +1,12 @@
+package Lab6.Ex62;
+
+public class Test
+{
+    private static int number_field=0;
+
+    public static void printNumberFieldAndIncrement()
+    {
+        System.out.println(number_field);
+        number_field += 1;
+    }
+}
