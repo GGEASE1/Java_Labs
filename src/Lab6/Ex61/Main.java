bdd26c6d131735925e42cf45e73fe3dc4c2fa5bd
kazan <kazantsev.06.2004@gmail.com>
2025-01-19 14:52:21 +0500

    Complete

---
+++ src/Lab6/Ex61/Main.java
@@ -0,0 +1,17 @@
+package Lab6.Ex61;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        char test_char = 'K';
+        String test_string = "hello";
+        char [] charArray = {'a', 'b', 'c', 'd', 'e'};
+        char [] charArraOne = {'f'};
+        Test test = new Test();
+        test.setCharOrStringField(test_char);
+        test.setCharOrStringField(test_string);
+        test.setCharOrStringField(charArray);
+        test.setCharOrStringField(charArraOne);
+    }
+}
