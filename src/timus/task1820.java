69de743e3de2a805d844224cd61bedd15f3187fe
kazan <kazantsev.06.2004@gmail.com>
2025-02-10 21:32:01 +0500

    Complete

---
+++ src/timus/task1820.java
@@ -0,0 +1,31 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1820
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int need_steaks = in.nextInt();
+        int max_steaks = in.nextInt();
+        if (max_steaks >= need_steaks)
+        {
+            out.print(2);
+        }
+        else
+        {
+            if ((need_steaks * 2) % max_steaks == 0)
+            {
+                out.print((need_steaks * 2) / max_steaks);
+            }
+            else
+            {
+                out.print((need_steaks * 2) / max_steaks +1);
+            }
+        }
+        out.flush();
+    }
+}
