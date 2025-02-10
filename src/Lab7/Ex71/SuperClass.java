cf4a91a987dd17c82026df28b0850e80ca12f64e
kazan <kazantsev.06.2004@gmail.com>
2025-02-10 20:39:06 +0500

    Complete

---
+++ src/Lab7/Ex71/SuperClass.java
@@ -0,0 +1,30 @@
+package Lab7.Ex71;
+
+public class SuperClass
+{
+    private String str1;
+
+    SuperClass(String strEx)
+    {
+        this.str1 = strEx;
+    }
+
+    SuperClass()
+    { }
+
+    @Override
+    public String toString()
+    {
+        String superClassNameAndFieldValue;
+        superClassNameAndFieldValue = "super" + "\n" +
+                "Class name: " + this.getClass().getSimpleName() + "\n" +
+                "str 1 = " + this.getStr1();
+        return superClassNameAndFieldValue;
+    }
+
+    public String getStr1()
+    {
+        return str1;
+    }
+
+}
