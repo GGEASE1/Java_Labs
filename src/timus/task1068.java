465d383419dc0192f3d899b40070728bed16a078
kazan <kazantsev.06.2004@gmail.com>
2025-02-16 17:16:29 +0500

    Complete

--- src/timus/task1068.java
+++ src/timus/task1068.java
@@ -8,9 +8,19 @@ public class task1068
     {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+        int result = 0;
 
-        long sum = (long)N * (N + 1) / 2;
-
-        System.out.println(sum);
+        if (N < 1)
+        {
+            for (int i = N; i <= 1; i++)
+            {
+                result = (N + 1) * (2 - (N)) / 2;
+            }
+        }
+        else
+        {
+            result = (1 + N) * N / 2;
+        }
+        System.out.println(result);
     }
 }
