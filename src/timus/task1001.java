5f212cecc6b4bae22bdc74b964998d7f33d6dc51
kazan <kazantsev.06.2004@gmail.com>
2025-02-16 15:57:25 +0500

    Complete

--- src/timus/task1001.java
+++ src/timus/task1001.java
@@ -7,7 +7,15 @@ public class task1001
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        String[] nums = in.nextLine().trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        while (in.hasNext())
+        {
+            sb.append(in.next());
+            sb.append(' ');
+        }
+
+        String[] nums = sb.toString().trim().split("\\s+");
 
         for (int i = nums.length - 1; i >= 0; i--)
         {
