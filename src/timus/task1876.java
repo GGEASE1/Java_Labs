fbda94b528f3fd7183558ab40a5af27f4d99e5ef
kazan <kazantsev.06.2004@gmail.com>
2025-02-16 13:56:10 +0500

    Complete

---
+++ src/timus/task1876.java
@@ -0,0 +1,17 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task1876
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int time = Math.max(2*b+40,2*a+39);
+
+        System.out.println(time);
+    }
+}
