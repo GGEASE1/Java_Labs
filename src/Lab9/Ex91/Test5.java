180a9048561c7e036d864fd0af1dcb4d8aa8cb14
kazan <kazantsev.06.2004@gmail.com>
2025-02-27 22:06:31 +0500

    Copmlete

--- src/Lab9/Ex91/Test5.java
+++ src/Lab9/Ex91/Test5.java
@@ -1,7 +1,6 @@
 package Lab9.Ex91;
 
-public class Test5
-{
+public class Test5 {
     public static void main(String[] args)
     {
         System.out.println(fact(8));
@@ -9,17 +8,23 @@ public class Test5
 
     public static int fact(int n)
     {
-      if(n==0)
-      {
-          return 0;
-      }
-      else if (n==1)
-      {
-          return 1;
-      }
-      else
-      {
-          return fact(n-2)+fact(n-1);
-      }
+        System.out.println("fact(" + n + ")");
+
+        if (n == 0)
+        {
+            System.out.println("Возвращено: 0");
+            return 0;
+        }
+        else if (n == 1)
+        {
+            System.out.println("Возвращено: 1");
+            return 1;
+        }
+        else
+        {
+            int res = fact(n - 2) + fact(n - 1);
+            System.out.println("Возвращено: " + res);
+            return res;
+        }
     }
 }
