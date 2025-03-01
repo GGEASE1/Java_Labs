bd37a187914faf3d68458e4a4baff15fb30b4511
kazan <kazantsev.06.2004@gmail.com>
2025-03-01 19:28:57 +0500

    Copmlete

---
+++ src/Lab9/Ex98/Cycle/Main.java
@@ -0,0 +1,45 @@
+package Lab9.Ex98.Cycle;
+
+public class Main
+{
+    {
+        MyList listHead = new MyList();
+        MyList listTail = new MyList();
+        String[] values = {"Kirill", "Danil", "Denis", "Vlad", "Daniel"};
+        System.out.println("Заполение списка с головы:");
+        listHead.createHead(values);
+        System.out.println(listHead.toString());
+        System.out.println("Заполение списка с хвоста:");
+        listTail.createTail(values);
+        System.out.println(listTail.toString());
+        System.out.println("Добавление элемента в начало списка:");
+        listHead.addFirst("Victor");
+        System.out.println(listHead.toString());
+        System.out.println("Добавление элемента в конец списка:");
+        listHead.addLast("Magnus");
+        System.out.println(listHead.toString());
+        System.out.println("Добавление элемента в указанное место списка:");
+        listHead.insert(2, "Linkoln");
+        System.out.println(listHead.toString());
+        System.out.println("Удаление первого элемента из списка:");
+        listHead.removeFirst();
+        System.out.println(listHead.toString());
+        System.out.println("Удаление последнего элемента из списка:");
+        listHead.removeLast();
+        System.out.println(listHead.toString());
+        System.out.println("Удаление элемента по индексу из списка:");
+        listHead.remove(3);
+        System.out.println(listHead.toString());
+        MyList listHeadRec = new MyList();
+
+        System.out.println("Заполение списка с головы c помощью рекурсии:");
+        listHeadRec.createHeadRec(values);
+        System.out.println(listHeadRec.toString());
+        MyList listTailRec = new MyList();
+        System.out.println("Заполение списка с хвоста с помощью рекурсии:");
+        listTailRec.createTailRec(values);
+        System.out.println(listTailRec.toString());
+        System.out.println("Вывод списка с помощью рекурсии:");
+        System.out.println(listTailRec.toStringRec());
+    }
+}
---
+++ src/Lab9/Ex98/Cycle/Node.java
@@ -0,0 +1,12 @@
+package Lab9.Ex98.Cycle;
+
+public class Node
+{
+    public String value;
+    public Node next;
+
+    Node(String value) {
+        this.value = value;
+        this.next = null;
+    }
+}
