6d66848a3b533068e1a50c33d612ecfe54d9c860
kazan <kazantsev.06.2004@gmail.com>
2025-04-13 19:26:09 +0500

    Copmlete

--- src/Lab12/Ex124/Main.java
+++ src/Lab12/Ex124/Main.java
@@ -1,4 +1,24 @@
 package Lab12.Ex124;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        List<Thread> tenThreads = new ArrayList<>();
+        for(int i=1; i<=10; i++)
+        {
+            int finalI = i;
+            tenThreads.add(new Thread(()->
+            {
+                Thread.currentThread().setName("Thread: " + String.valueOf(finalI));
+                System.out.println(Thread.currentThread().getName());
+            }));
+        }
+
+        tenThreads.forEach(Thread::start);
+    }
 }
